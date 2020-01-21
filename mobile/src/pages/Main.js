9df0f398e6598bbd841eb507c3295cf2dfe20ac9
Gamonpo <gamonpo@outlook.com>
2020-01-21 17:24:23 -0200

    Redireciona para perfil no github

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -4,7 +4,7 @@ import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from "expo-location";
 
 
-function Main() {
+function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null);
 
     useEffect(() => {
@@ -41,7 +41,9 @@ function Main() {
             <Marker coordinate={ { latitude: -16.7117328, longitude: -49.2825592 } } >
                 <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/34238796?s=460&v=4' }} />
 
-                <Callout>
+                <Callout onPress={ () => {
+                    navigation.navigate('Profile', { github_username: 'gamonpo' });
+                }}>
                     <View style={styles.callout}>
                         <Text style={styles.devName}>Gabriel Monteiro</Text>
                         <Text style={styles.devBio}>Apaixonado por tecnologias!</Text>
